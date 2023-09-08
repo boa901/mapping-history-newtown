e5ee7ebd8084e2c01832dd7b389c06eb5ca97253
boa901 <phillydooper@gmail.com>
2023-09-07 21:01:30 -0400

    Set Map position, bounds, and zoom
    In the Map component, set the MapContainer props to set max zoom, min
    zoom, center, and bounds. This shows the user the map of harrisonburg,
    restricting scrolling far away from the city.

--- next/modules/map/components/Map.tsx
+++ next/modules/map/components/Map.tsx
@@ -1,19 +1,31 @@
 'use client';
 
+import L from 'leaflet';
 import { MapContainer, TileLayer } from 'react-leaflet';
 
 export default function Map() {
+
+  const bounds = new L.LatLngBounds(
+    new L.LatLng(38.40087424, -78.91065653),
+    new L.LatLng(38.49355771, -78.82668815),
+  );
+
   return (
     <MapContainer
-      center={[0, 0]}
-      zoom={2}
+      center={[38.447215975, -78.86867234]}
+      zoom={14}
+      maxBounds={bounds}
+      maxZoom={20}
+      minZoom={12}
     >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        maxZoom={19}
       />
       <TileLayer
         url="http://localhost:3000/tiles/{z}/{x}/{y}.png"
+        maxZoom={20}
       />
     </MapContainer>
   );

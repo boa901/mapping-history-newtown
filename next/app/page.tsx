7e7ba49262f6305922f57d347d1f69ed36a1f35b
boa901 <phillydooper@gmail.com>
2023-11-14 15:16:02 -0500

    Add household view to home page
    Add the household view component to the home page. Set a household based
    on which house is selected in the map by creating a page state variable
    that stores address information. This allows the user to view resident
    data when they click on a house.

--- next/app/page.tsx
+++ next/app/page.tsx
@@ -1,9 +1,19 @@
+'use client';
+
 import dynamic from 'next/dynamic';
+import { useState } from 'react';
+
+import HouseholdView from '@/modules/common/components/HouseholdView';
 
 const Map = dynamic(() => import('@/modules/map/components/Map'), { ssr: false });
 
 export default function Home() {
+  const [selectedHouse, setSelectedHouse] = useState<{street_no: string, street_name: string}>();
+
   return (
-    <Map />
+    <>
+      <Map select={setSelectedHouse} />
+      <HouseholdView house={selectedHouse} />
+    </>
   );
 }

ffaa1c44761f13205c49d64fa65d11b6dc389f03
boa901 <phillydooper@gmail.com>
2023-12-05 14:32:58 -0500

    Use grid layout on main page
    Use tailwind css grid with two columns to layout the page side by side.

--- next/app/page.tsx
+++ next/app/page.tsx
@@ -8,12 +8,19 @@ import HouseholdView from '@/modules/common/components/HouseholdView';
 const Map = dynamic(() => import('@/modules/map/components/Map'), { ssr: false });
 
 export default function Home() {
-  const [selectedHouse, setSelectedHouse] = useState<{street_no: string, street_name: string} | null>();
+  const [selectedHouse, setSelectedHouse] = useState<{
+    street_no: string,
+    street_name: string,
+  } | null>();
 
   return (
-    <div className="map-data-panel-view">
-      <Map select={setSelectedHouse} />
-      <HouseholdView house={selectedHouse} />
+    <div className="grid grid-cols-2 h-full">
+      <div className="w-full">
+        <Map select={setSelectedHouse} />
+      </div>
+      <div className="w-full">
+        <HouseholdView house={selectedHouse} />
+      </div>
     </div>
   );
 }

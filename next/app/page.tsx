7656463cc23de2bb5846b7a8ece7fee7b1b9c7fc
boa901 <phillydooper@gmail.com>
2023-12-05 15:11:10 -0500

    Remove HouseholdView from the main page
    Remove the HouseholdView component from the main page. Show only the map
    on the full page.

--- next/app/page.tsx
+++ next/app/page.tsx
@@ -4,8 +4,6 @@ import dynamic from 'next/dynamic';
 import { useState } from 'react';
 import { Button, RangeSlider } from 'flowbite-react';
 
-import HouseholdView from '@/modules/common/components/HouseholdView';
-
 const Map = dynamic(() => import('@/modules/map/components/Map'), { ssr: false });
 
 export default function Home() {
@@ -15,19 +13,20 @@ export default function Home() {
   } | null>();
 
   return (
-    <div className="grid grid-cols-2 h-full">
-      <div className="w-full flex flex-col">
-        <Map select={setSelectedHouse} />
-        <div className="flex flex-row justify-between items-center p-2 bg-gray-200/50">
-          <Button>Show Residents</Button>
-          <div className="flex flex-row">
-            <h1 className="mr-2">Opacity:</h1>
-            <RangeSlider />
-          </div>
+    <div className="w-full flex flex-col p-2">
+      <Map select={setSelectedHouse} />
+      <div className="flex flex-row justify-between items-center p-2 bg-gray-200/50">
+        <div>
+          {selectedHouse ? (
+            <Button>Show Residents</Button>
+          ) : (
+            <Button disabled>Show Residents</Button>
+          )}
         </div>
-      </div>
-      <div className="w-full">
-        <HouseholdView house={selectedHouse} />
+        <div className="flex flex-row">
+          <h1 className="mr-2">Opacity:</h1>
+          <RangeSlider />
+        </div>
       </div>
     </div>
   );

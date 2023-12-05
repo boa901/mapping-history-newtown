9fb68cd10ef87f7f59a2698e2e80ada9c1e6f32b
boa901 <phillydooper@gmail.com>
2023-12-05 14:47:27 -0500

    Add flowbite-react to the project
    Add flowbite-react to the project to use prebuilt components for basic
    functions and page layout.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,10 +5,11 @@ module.exports = {
     "./next/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./next/modules/components/*.{js,ts,jsx,tsx,mdx}",
     "./next/common/components/*.{js,ts,jsx,tsx,mdx}",
+    "node_modules/flowbite-react/lib/esm/**/*.js",
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require('flowbite/plugin')],
 }
 
